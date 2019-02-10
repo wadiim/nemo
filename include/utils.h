6576edcf00190f0d2e13c68793fea12e88e07dc2
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2019-02-11 00:04:26 +0100

    Cleanup

--- include/utils.h
+++ include/utils.h
@@ -9,7 +9,6 @@ std::string strip(std::string& str);
 std::string strip(std::string&& str);
 bool is_file_exist(const std::string& path);
 size_t string_to_size_t(const std::string& str);
-void escape_character(std::string& str, const char ch);
 void unescape_character(std::string& str, const char ch);
 std::vector<std::string> split(const std::string& s, char delimiter);
 size_t find_first_nonescaped(const std::string& str, char ch);
